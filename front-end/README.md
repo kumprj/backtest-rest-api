355ad5ae53d34a56bad12d9c610a2d334f8b9fe3
Robert Kump <kumprj@miamioh.edu>
2019-11-29 11:00:47 -0500

    initial push

---
+++ front-end/README.md
@@ -0,0 +1,7 @@
+Andrew unkowns:
+* Favicon
+* Title of size
+* Styling needs to be refactored
+* CORS is blocking requests 
+  * https://serverless.com/blog/cors-api-gateway-survival-guide/
+  * theres a cors block in serverless.yml or add it in the header on response in handler.js Not entirely sure what the solution is, but need to debug it.
---
+++ handler.js
@@ -0,0 +1,84 @@
+const db = require('./db_connect');
+
+module.exports.getAllTodos = (event, context, callback) => {  
+  context.callbackWaitsForEmptyEventLoop = false;  
+  db.getAll('backtest_results_stg')
+    .then(res => {
+      callback(null, {
+        statusCode: 200,
+        body: JSON.stringify(res)
+      })
+    })
+    .catch(e => {
+      console.log(e);
+      callback(null, {
+        statusCode: e.statusCode || 500,
+        body: 'Error: Could not find Todos: ' + e
+      })
+    })
+};
+
+   String.prototype.replaceAll = function (stringToFind, stringToReplace) {
+        if (stringToFind === stringToReplace) return this;
+        var temp = this;
+        var index = temp.indexOf(stringToFind);
+        while (index != -1) {
+            temp = temp.replace(stringToFind, stringToReplace);
+            index = temp.indexOf(stringToFind);
+        }
+        return temp;
+  };
+module.exports.getResults = async (event, context, callback) => {  
+
+  context.callbackWaitsForEmptyEventLoop = false;  
+  try {
+    var input = event.pathParameters.todays_date
+    var date = input.replaceAll("_", "/")
+    console.log(date)
+    const sql = "select symbol, indicator_level, avg_return, win_rate, avg_return, avg_win, avg_loss, observation_period from backtest_results_stg where todays_date = $1 order by symbol, cast(left(observation_period, position(' ' IN observation_period)-1) as int)"
+    const result = await db.query(sql, date)
+	  .then(res => {
+		callback(null, {
+			statusCode: 200,
+			body: JSON.stringify(res)
+		})
+	  });
+    //console.log(result)
+   } catch (e) {
+	console.log(e)
+   }
+   
+  /**
+  db.getById('backtest_results_stg', event.pathParameters.total_pos)
+    .then(res => {
+      callback(null,{
+        statusCode: 200,
+        body: JSON.stringify(res)
+      })
+    })
+    .catch(e => {
+      callback(null,{
+        statusCode: e.statusCode || 500,
+        body: "Could not find Todo: " + e
+      })
+    })
+    */
+};
+
+
+module.exports.hello = async event => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: 'Go Serverless v1.0! Your function executed successfully!',
+        input: event,
+      },
+      null,
+      2
+    ),
+  };
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
