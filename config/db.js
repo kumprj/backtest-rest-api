363e245620d8d71feeb977baa3d8c2acaf0fc616
ndesai <ndesai@hmbnet.com>
2020-01-06 13:20:47 -0500

    Set up AWS Secret Manager to store our API Key and db credentials. serverless.yml now pulls those values and stores them as environment variables. Should allow for CI with github.

---
+++ config/db.js
@@ -0,0 +1,7 @@
+module.exports = {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
+};
